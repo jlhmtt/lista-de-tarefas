8ff6f526bf58de35c06ab397d8c9e3afbea5b039
julia <julhacolucci@gmail.com>
2023-02-10 13:55:55 -0300

    ao apertar enter no input, adicionar item à lista

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-let coisas = []
-
+let coisas = [];
 
 let tamanhoLista = coisas.length;
 
-let textoAdicionar = document.getElementById('textoAdicionar')
+let input = document.getElementById("input");
 
-let lista = document.getElementById('lista')
+let lista = document.getElementById("lista");
 
-let contadorBotaoId = 0
+let contadorBotaoId = 0;
 
 // função pra adicionar itens à lista
 function adicionar() {
-
   // se a caixa de texto não estiver vazia,
   // adiciona o conteúdo ao fim da lista e apaga o texto da caixa
   // adiciona li ao ul
   // adiciona no fim de cada li um botão com uma id específica (criada por um contador)
-  if (textoAdicionar.value != "") {
+  if (input.value != "") {
     contadorBotaoId++;
-    coisas.push(textoAdicionar.value);
-    lista.innerHTML += `<li id="li${contadorBotaoId}"> ${textoAdicionar.value}  <button onclick="remover(${contadorBotaoId})">X</button</li>`
-    textoAdicionar.value = "";
+    coisas.push(input.value);
+    lista.innerHTML += `<li id="li${contadorBotaoId}"> ${input.value}  <button onclick="remover(${contadorBotaoId})">X</button</li>`;
+    input.value = "";
   }
 }
 
+// função pra adicionar itens à lista ao pressionar Enter no input
+input.addEventListener("keyup", function remover(event) {
+  // quando pressionar Enter dentro do input, chama a função adicionar()
+  if (event.key == "Enter") {
+    event.preventDefault();
+    adicionar();
+  }
+});
+
 // função pra remover itens da lista
 function remover(id) {
-
   // a função usa como input a id adicionada a cada li
-  // essa id é também o index do array coisas[], então é possível deletar o item do array e do HTML usando o mesmo index 
-  delete coisas[id]
-  let li = document.getElementById(`li${id}`)
-  li.remove()
+  // essa id é também o index do array coisas[], então é possível deletar o item do array e do HTML usando o mesmo index
+  delete coisas[id];
+  let li = document.getElementById(`li${id}`);
+  li.remove();
 }
